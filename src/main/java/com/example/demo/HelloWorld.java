2ac6aef4bdb7936864bf7e999d41c791908b3612
Yanjun Zhao <yanjun.zhao@flexport.com>
2021-01-07 13:42:05 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class HelloWorld {
+    private String name;
+
+    public HelloWorld(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
